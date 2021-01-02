f48a5cd406e444845b97c1c6fc8264b534c84060
mickersicc <mickersicc@gmail.com>
2021-01-02 18:46:51 -0500

    added grocery list component

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
+import { withAuthenticator } from '@aws-amplify/ui-react';
+
 import './App.css';
+import GroceryList from './grocery-list/GroceryList';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <GroceryList />
     </div>
   );
 }
 
-export default App;
+export default withAuthenticator(App);
---
+++ src/grocery-list/GroceryList.tsx
@@ -0,0 +1,33 @@
+import React, { useEffect, useState } from 'react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { listItems } from '../graphql/queries';
+
+const GroceryList = () => {
+  const [items, setItems] = useState<any[]>([]);
+
+  useEffect(() => {
+    fetchItems();
+  }, []);
+
+  async function fetchItems() {
+    try {
+      const itemsData: any = await API.graphql(graphqlOperation(listItems));
+      const items = itemsData.data.listItems.items;
+
+      setItems(items);
+    } catch (error) {
+      console.log('fetchItems', error);
+    }
+  }
+
+  return (
+    <div>
+      <div>Grocery List</div>
+      {items.map((item) => (
+        <div>{item.name}</div>
+      ))}
+    </div>
+  );
+};
+
+export default GroceryList;
